b8e39ba0d511bef9c655d5b13bc0e439f0c7adc9
Owhen Min <thwn911@gmail.com>
2024-11-27 06:21:05 +0900

    plz..

--- back/settings.py
+++ back/settings.py
@@ -7,7 +7,6 @@ import os, json
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-secret = os.path.join(BASE_DIR, 'secret.json')
 with open(secret) as f:
     secrets = json.loads(f.read())
 
@@ -17,10 +16,8 @@ def get_secret(keyword, secrets=secrets):
     except KeyError:
         raise ImproperlyConfigured("No variable : {}".format(keyword))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+secret = os.path.join(BASE_DIR, 'secret.json')
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_secret("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
